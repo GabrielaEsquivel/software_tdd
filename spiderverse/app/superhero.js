8474bec24222dd0a2a2c86f3ade4784729aa7b6f
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-22 18:37:07 -0500

    Paso 3: Creacion de la clase Spiderman

---
+++ spiderverse/app/superhero.js
@@ -0,0 +1,11 @@
+class Spiderman{
+    constructor(name, age, actor, numberOfMovies, filmStudio){
+        this.nameHero = name;
+        this.ae = age;
+        this.actor = actor;
+        this.numberOfMovies = numberOfMovies;
+        this.filmStudio = filmStudio;
+    }
+}
+
+module.exports = Spiderman; //exportar un modulo
